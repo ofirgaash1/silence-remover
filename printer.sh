437c09bc3c195fbb4cb10620665271bf390d9977
Ofir Gaash <77354079+ofirgaash1@users.noreply.github.com>
2025-05-10 01:00:21 +0300

    Create printer.sh

---
+++ printer.sh
@@ -0,0 +1,15 @@
+ofir@ofir-Latitude-7420:~$ cat printer
+#!/bin/bash
+
+# Check if directory is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+# Recursively find all regular files, excluding hidden files and dirs
+find "$1" -type d -name '.*' -prune -o -type f -name '.*' -prune -o -type f -print | while read -r file; do
+  echo "##### FILE: $file #####"
+  cat "$file"
+  echo -e "\n########################\n"
+done
