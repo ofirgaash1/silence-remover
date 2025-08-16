0b98534a501e5d5f0077ffa194dd4559499c83c1
Ofir Gaash <77354079+ofirgaash1@users.noreply.github.com>
2025-08-16 12:53:17 +0300

    Update vite.config.mjs

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -7,7 +7,7 @@ export default {
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html', 'tones.html')
       }
     }
   },
